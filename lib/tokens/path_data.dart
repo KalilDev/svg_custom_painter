72d4780af8c1adc725e210957cd4cce4f4412562
KalilDev <kalildev@users.noreply.github.com>
2019-12-18 13:13:36 -0300

    path_data: Allow lerping any 2 paths!

--- lib/tokens/path_data.dart
+++ lib/tokens/path_data.dart
@@ -1,58 +1,66 @@
+import 'package:svg_custom_painter/rendering/path_simplifier_node.dart';
+
 import 'path_data_operations.dart';
 import 'package:meta/meta.dart';
 import '../utils.dart';
-
 class PathData {
   const PathData(this.operations, {@required this.x, @required this.y});
   final List<PathOperation> operations;
   final double x;
   final double y;
 
+  @override
+  int get hashCode => y.hashCode+x.hashCode+operations.hashCode;
+
+  @override
+  String toString() {
+    return operations.map((PathOperation o)=>o.toString()).reduce((String s1, String s2)=>s1 + ' ' + s2);
+  }
+
+  static final PathSimplifierNode pathSimplifier = PathSimplifierNode();
   static PathData lerp(PathData a, PathData b, double t) {
     assert(a != null);
     assert(b != null);
     assert(t != null);
-    assert(a.operations.length == b.operations.length);
+    bool isSimple(PathOperation o) => o is CubicBezier || o is Move || o is Close;
+    // Needed so the lists are growable.
+    List<PathOperation> aOps = List.from(a.operations);
+    List<PathOperation> bOps = List.from(b.operations);
+    if (!(aOps.every(isSimple) && bOps.every(isSimple))) {
+      // Simplify using an PathSimplifierNode
+      aOps = pathSimplifier.simplify(aOps);
+      bOps = pathSimplifier.simplify(bOps);
+    }
+    if (aOps.length != bOps.length) {
+      // Add dummy cubics at the start. This does NOT result in the best possible morph!
+      // Probably inserting these at random indexes will look better.
+      // Inserting at regular intervals wil look even better prob.
+      // TODO: Experiment with these
+      final int diff = (aOps.length - bOps.length).abs();
+      PathOperation genCubics(PathOperation first) => CubicBezier(alias: 'C', x1: first.x, y1: first.y, x2: first.x, y2: first.y, x: first.x, y: first.y);
+      if (aOps.length > bOps.length) {
+        final PathOperation first = bOps.first;
+        bOps.insertAll(1, Iterable.generate(diff, (int i)=>genCubics(first)));
+      } else {
+        final PathOperation first = aOps.first;
+        aOps.insertAll(1, Iterable.generate(diff, (int i)=>genCubics(first)));
+      }
+    }
+
     List<PathOperation> lerp = [];
-    for (int i = 0; i < a.operations.length; i++) {
-      final PathOperation o1 = a.operations[i];
-      final PathOperation o2 = b.operations[i];
+    for (int i = 0; i < aOps.length; i++) {
+      final PathOperation o1 = aOps[i];
+      final PathOperation o2 = bOps[i];
       PathOperation result;
       assert(o1.runtimeType == o2.runtimeType);
-      switch (o1.runtimeType) {
-        case Move:
-          result = Move.lerp(o1, o2, t);
-          break;
-        case Close:
-          result = Close.lerp(o1, o2, t);
-          break;
-        case Line:
-          result = Line.lerp(o1, o2, t);
-          break;
-        case HorizontalLine:
-          result = HorizontalLine.lerp(o1, o2, t);
-          break;
-        case VerticalLine:
-          result = VerticalLine.lerp(o1, o2, t);
-          break;
-        case CubicBezier:
-          result = CubicBezier.lerp(o1, o2, t);
-          break;
-        case SmoothCubicBezier:
-          result = SmoothCubicBezier.lerp(o1, o2, t);
-          break;
-        case QuadraticBezier:
-          result = QuadraticBezier.lerp(o1, o2, t);
-          break;
-        case SmoothQuadraticBezier:
-          result = SmoothQuadraticBezier.lerp(o1, o2, t);
-          break;
-        case Arc:
-          result = Arc.lerp(o1, o2, t);
-          break;
-      }
+      if (o1 is Move)
+        result = Move.lerp(o1, o2, t);
+      if (o1 is Close)
+        result = Close.lerp(o1, o2, t);
+      if (o1 is CubicBezier)
+        result = CubicBezier.lerp(o1, o2, t);
       lerp.add(result);
     }
     return PathData(lerp, x: lerpDouble(a.x, b.x, t), y: lerpDouble(a.y, b.y, t));
-  }
+}
 }
